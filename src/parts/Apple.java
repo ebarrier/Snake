5b42b909b12de9fe99a43004e822dc6fcc4508db
ebarrier <etienne.barrier@gmail.com>
2015-12-07 17:53:25 +0200

    Apple get random location

--- src/parts/Apple.java
+++ src/parts/Apple.java
@@ -4,19 +4,20 @@ import javafx.scene.paint.Color;
 
 public class Apple extends Block {
 
-	static int x = (int)(Math.random()*1000);
-	static int y = (int)(Math.random()*500);
-	
 	public Apple() {
-		new Block(x,y);
-		setWidth(SIZE);
-		setHeight(SIZE);
-		setArcWidth(0);
-		setArcHeight(0);
+		super(getRandomX(), getRandomY());
 		setFill(Color.RED);
 	}
 	
+	public static int getRandomX() {
+		int x = (int)(Math.random()*1000/SIZE);
+		return x;
+	}
 	
+	public static int getRandomY(){
+		int y = (int)(Math.random()*500/SIZE);
+		return y;
+	}
 	
 
 }
